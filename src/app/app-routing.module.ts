66ef83206f67e525e61bd42679871c5a63764c66
alexbyd <alex.baena@est.iudigital.edu.co>
2021-09-13 22:06:44 -0500

    modificamos el archivo app.comp.html para agregar los botones y trate de centrarlos con el css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { IngresoComponent } from './casilla/ingreso/Ingreso.component.ts';
+//import { SalidaComponent } from './casilla/salida/Salida.component.ts';
+//import { CheckComponent } from './casilla/check/Check.component.ts';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: "ingreso", component:IngresoComponent},
+  //{path: "salida", component:SalidaComponent},
+  //{path: "check", component:CheckComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresoComponent } from './casilla/ingreso/ingreso.component';
+import { SalidaComponent } from './casilla/salida/salida.component';
+import { CheckComponent } from './casilla/check/check.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent,
+    SalidaComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
